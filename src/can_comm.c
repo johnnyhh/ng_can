353163524f12d1027853cc8b927f1708b4097d23
John Horne <jhhorne@gmail.com>
2017-03-14 15:54:02 -0700

    added 'open' functionality to ng_can.c

---
+++ src/can_comm.c
@@ -0,0 +1,69 @@
+#include "can_comm.h"
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <sys/file.h>
+#include <time.h>
+#include <poll.h>
+
+#include <sys/socket.h>
+#include <net/if.h>
+
+#include <linux/can.h>
+#include <linux/can/raw.h>
+
+
+
+int can_init(struct can_port **pport)
+{
+    struct can_port *port = malloc(sizeof(struct can_port));
+    *pport = port;
+
+    port->fd = -1;
+    return 0;
+}
+
+int can_is_open(struct can_port *port)
+{
+    return port->fd != -1;
+}
+
+int can_close(struct can_port *port)
+{
+  close(port->fd);
+  port->fd = -1;
+  return 0;
+}
+
+int can_open(struct can_port *can_port, char *interface_name)
+{
+  int s;
+  struct sockaddr_can addr;
+  struct ifreq ifr;
+
+  //open socket
+  if ((s = socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0)
+    return ERL_DRV_ERROR_ERRNO;
+
+  int flags = fcntl(s, F_GETFL, 0);
+  fcntl(s, F_SETFL, flags | O_NONBLOCK);
+
+  can_port->fd = s;
+
+  //get interface index
+  strcpy(ifr.ifr_name, interface_name);
+  ioctl(s, SIOCGIFINDEX, &ifr);
+
+  //bind
+  addr.can_family = AF_CAN;
+  addr.can_ifindex = ifr.ifr_ifindex;
+
+  return bind(s, (struct sockaddr *)&addr, sizeof(addr));
+}
--- src/ng_can.c
+++ src/ng_can.c
@@ -4,6 +4,7 @@
 
 #include "erlcmd.h"
 #include "util.h"
+#include "can_comm.h"
 
 #include <poll.h>
 #include <unistd.h>
@@ -11,13 +12,98 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+static struct can_port *can_port = NULL;
+
+static struct request_handler request_handlers[] = {
+{ "open", handle_open },
+{ NULL, NULL }
+};
+
+// Utilities
+static const char response_id = 'r';
+static const char notification_id = 'n';
+
+/**
+ * @brief Send :ok back to Elixir
+ */
+static void send_ok_response()
+{
+    char resp[256];
+    int resp_index = sizeof(uint16_t); // Space for payload size
+    resp[resp_index++] = response_id;
+    ei_encode_version(resp, &resp_index);
+    ei_encode_atom(resp, &resp_index, "ok");
+    erlcmd_send(resp, resp_index);
+}
+
+/**
+ * @brief Send a response of the form {:error, reason}
+ *
+ * @param reason a reason (sent back as an atom)
+ */
+static void send_error_response(const char *reason)
+{
+    char resp[256];
+    int resp_index = sizeof(uint16_t); // Space for payload size
+    resp[resp_index++] = response_id;
+    ei_encode_version(resp, &resp_index);
+    ei_encode_tuple_header(resp, &resp_index, 2);
+    ei_encode_atom(resp, &resp_index, "error");
+    ei_encode_atom(resp, &resp_index, reason);
+    erlcmd_send(resp, resp_index);
+}
+
+static void handle_elixir_request(const char *req, void *cookie)
+{
+    (void) cookie;
+
+    // Commands are of the form {Command, Arguments}:
+    // { atom(), term() }
+    int req_index = sizeof(uint16_t);
+    if (ei_decode_version(req, &req_index, NULL) < 0)
+        errx(EXIT_FAILURE, "Message version issue?");
+
+    int arity;
+    if (ei_decode_tuple_header(req, &req_index, &arity) < 0 ||
+            arity != 2)
+        errx(EXIT_FAILURE, "expecting {cmd, args} tuple");
+
+    char cmd[MAXATOMLEN];
+    if (ei_decode_atom(req, &req_index, cmd) < 0)
+        errx(EXIT_FAILURE, "expecting command atom");
+
+    for (struct request_handler *rh = request_handlers; rh->name != NULL; rh++) {
+        if (strcmp(cmd, rh->name) == 0) {
+            rh->handler(req, &req_index);
+            return;
+        }
+    }
+    errx(EXIT_FAILURE, "unknown command: %s", cmd);
+}
+
+static void handle_open(const char *req, int *req_index)
+{
+    char interface_name[64];
+    if(ei_decode_binary(req, req_index, name, &binary_len) < 0) {
+        send_error_response("enoent");
+        return;
+    }
+    name[term_size] = '\0';
+
+    // If the uart was already open, close and open it again
+    if (can_is_open(can_port))
+        can_close(can_port);
+
+    if (can_open(can_port, interface_name) >= 0) {
+        send_ok_response();
+    } else {
+        send_error_response("error opening can port");
+    }
+}
+
 int main()
 {
-    uart_default_config(&current_config);
-    if (uart_init(&uart,
-                  handle_write_completed,
-                  handle_read_completed,
-                  handle_notify_read) < 0)
+    if (can_init(&can_port) < 0)
         errx(EXIT_FAILURE, "uart_init failed");
 
     struct erlcmd *handler = malloc(sizeof(struct erlcmd));
@@ -30,26 +116,10 @@ int main()
         fdset[0].events = POLLIN;
         fdset[0].revents = 0;
 
-        int timeout = -1; // Wait forever unless told by otherwise
-        int count = uart_add_poll_events(uart, &fdset[1], &timeout);
-
-        int rc = poll(fdset, count + 1, timeout);
-        if (rc < 0) {
-            // Retry if EINTR
-            if (errno == EINTR)
-                continue;
-
-            err(EXIT_FAILURE, "poll");
-        }
-
         if (fdset[0].revents & (POLLIN | POLLHUP)) {
             if (erlcmd_process(handler))
                 break;
         }
-
-        // Call uart_process if it added any events
-        if (count)
-            uart_process(uart, &fdset[1]);
     }
 
     // Exit due to Erlang trying to end the process.
